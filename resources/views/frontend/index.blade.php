e6a6d20c3deb1db8b9be5dbb5791e9a12ff3ccc4
QuanDo <quan.dm@teko.vn>
2025-02-26 14:45:53 +0700

    update

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -62,12 +62,12 @@
             </section>
         @endif
         @if ($newsBlockPosts = \App\Helpers::getIndexNewsBlockCategoryWithPosts())
+                    <?php dd( $newsBlockPosts); ?>
         <section class="categories block">
             <div class="container">
                 <h2 class="block-title">{{ trans('frontend.category') }}</h2>
                 <div class="categories-tabs">
                     @foreach ($indexFeaturePosts as $index => $info)
-                        <?php dd( $info); ?>
                         <button class="tab-btn {{ $index == 0 ? 'active': "" }}" data-tab="cate{{ $info['cate']->id }}">{{$info['cate']->name}}</button>
                     @endforeach
                     <button class="tab-btn more-btn" onclick="window.location.href='/news-list.html'">+</button>
